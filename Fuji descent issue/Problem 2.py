f40f30625936e2eddd15a31eca6ad113fa030309
LittleZealot <144759191+LittleZealot@users.noreply.github.com>
2023-11-15 11:00:32 +0800

    Update Problem 2.py

--- Fuji descent issue/Problem 2.py
+++ Fuji descent issue/Problem 2.py
@@ -1,36 +1,21 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 
 file_path = "data/mtfuji_data.csv"
-df = pd.read_csv(file_path)
+data = pd.read_csv(file_path)
+def calculate_gradient(data, point_number):
 
-plt.figure(figsize=(10, 6))
-plt.plot(df["x"], df["elevation"], linestyle='-')
-plt.title("Mt. Fuji")
-plt.xlabel("Position")
-plt.ylabel("Elevation [m]")
-plt.grid(True)
-plt.show()
-
-def calculate_gradient(df, current_point):
-    if current_point >= 1 and current_point < len(df):
-        elevation_current = df.loc[current_point, "elevation"]
-        elevation_previous = df.loc[current_point - 1, "elevation"]
-        delta_points = df.loc[current_point, "x"] - df.loc[current_point - 1, "x"]
-        delta_elevation = elevation_current - elevation_previous
-
-        if delta_points != 0:
-            gradient = delta_elevation / delta_points
-            return gradient
-        else:
-            print("Delta points is zero. Unable to calculate gradient.")
-            return None
-    else:
-        print("Invalid current point. Please provide a valid point.")
+    current_point = data[data['x'] == point_number]
+    previous_point_number = point_number - 1
+    previous_point = data[data['x'] == previous_point_number]
+    if previous_point.empty:
+        print(f"Error: Point {previous_point_number} not found in the dataset.")
         return None
+    delta_elevation = current_point['elevation'].values[0] - previous_point['elevation'].values[0]
+    delta_point_number = current_point['x'].values[0] - previous_point_number
+    gradient = delta_elevation / delta_point_number
 
-current_point = 137
-resulting_gradient = calculate_gradient(df, current_point)
+    return gradient
 
-if resulting_gradient is not None:
-    print(f"Gradient at point {current_point}: {resulting_gradient}")
+point_number = 136
+gradient_at_point_5 = calculate_gradient(data, point_number)
+print(f"Gradient at point {point_number}: {gradient_at_point_5}")
